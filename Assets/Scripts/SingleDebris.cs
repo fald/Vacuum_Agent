757cbdc800f81a4ffcf2285d6adaaf23869e4184
Firas <4348649+fald@users.noreply.github.com>
2022-04-04 17:08:55 -0400

    Fixing package versions - stuck on updates, but should be good for now, at least.

---
+++ Assets/Scripts/SingleDebris.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Unity.MLAgents;
+using Unity.MLAgents.Actuators;
+
+
+public class NewBehaviourScript : Agent
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
