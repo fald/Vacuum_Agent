f22e208179ab76edb3abe8468ae05d6bb8107744
Firas <4348649+fald@users.noreply.github.com>
2022-04-05 19:36:31 -0400

    Script:OnEpisodeBegin.

--- Assets/Scripts/SingleDebris.cs
+++ Assets/Scripts/SingleDebris.cs
@@ -26,4 +26,13 @@ public class SingleDebris : Agent
         rigidbody = GetComponent<Rigidbody>();
     }
 
+    public override void OnEpisodeBegin()
+    // This is called at the beginning of each episode. So when max step counter is achieved.
+    {
+        goal.SetActive(true); // Reset the goal.
+        transform.position = startPosition; // Reset the agent.
+        debrisPosition.y = 0.25f; // Reset the debris 
+        goal.transform.position = debrisPosition + Quaternion.Euler(Vector3.up * Random.Range(0f, 360f)) * Vector3.forward * 5f; // Reset the goal...but in a random location.
+    }
+
 }
