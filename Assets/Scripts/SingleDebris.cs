49acf22ab907f0b0dd1dd4f989ec6f8017b837d3
Firas <4348649+fald@users.noreply.github.com>
2022-04-05 19:22:47 -0400

    Script:Initialize method.

--- Assets/Scripts/SingleDebris.cs
+++ Assets/Scripts/SingleDebris.cs
@@ -10,4 +10,20 @@ public class SingleDebris : Agent
 {
     [SerializeField] private GameObject goal;
 
+    private int counter = 1;
+    private float moveSpeed = 3f;
+    private float turnSpeed = 380f;
+    private Vector3 startPosition;
+    private Vector3 debrisPosition;
+    new private Rigidbody rigidbody;
+
+
+    public override void Initialize()
+    // This gets called once at the beginning.
+    {
+        debrisPosition = transform.position; // Sneaky later things.
+        startPosition = transform.position;
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
 }
